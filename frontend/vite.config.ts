c8f54b15815707cc8f3b5da159b9f946d5dbb5e1
pebblepark <subling02@gmail.com>
2022-07-05 15:37:28 +0900

    Feat: github-pages 배포 추가

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,10 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:5000',
-    },
+  build: {
+    outDir: 'build',
   },
 });
